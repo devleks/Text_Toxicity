7cd3213ce8c8d85668fadd9ae0877dffa740e163
devleks <elekia.ochola@gmail.com>
2025-03-27 22:33:45 +0000

    reviewes and worked on json output

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ from gradio_client import Client
 import matplotlib.pyplot as plt
 import base64
 from io import BytesIO
+import json
 
 st.title(" Text Toxicity App")
 st.write( "😃 Welcome To The Friendly Text Moderation")
@@ -30,7 +31,14 @@ def display_outcome(encoded_image):
     ax.axis('off')  # Hide axes
 
     return fig
-    
+
+def process_metrics(text):
+    # Join the list into a single JSON string
+    json_string = "".join(text)
+
+    # Convert the string into a JSON object
+    json_data = json.loads(json_string)
+    return json_data
 
 review_text = st.text_area("Text to reviewed",
                             placeholder="Enter text for review",
@@ -40,7 +48,8 @@ safety = st.slider('Personalize Safer Value: (larger value is less safe)',
                    min_value=0.005,max_value=0.1,value=0.02,step=0.005)
 
 eval_result = get_toxic(review_text,safety)
-
+processed_metrics = process_metrics(eval_result[1])
 
 st.pyplot(display_outcome((eval_result[0]['plot'][23::])))
 
+st.dataframe(processed_metrics,column_config={'':'Key','value':'Value'})
