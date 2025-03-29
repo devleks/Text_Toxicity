f9787ad172f4f8a3c48149616482fa517e14b15a
devleks <elekia.ochola@gmail.com>
2025-03-29 20:15:22 +0000

    update code with API to fetch tweets

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,21 +40,63 @@ def process_metrics(text):
     json_data = json.loads(json_string)
     return json_data
 
-review_text = st.text_area("Text to reviewed",
-                            placeholder="We in these hot ass Texas streets, honey. Y’all know we got Governor Hot Wheels down there, Come on now! And the only thing hot about him is that he is a hot ass mess, honey! So, um, so yes! Yes, yes, yes.",
-                            )
+def get_tweets():
+    client = Client("duchaba/Friendly_Text_Moderation")
+    result = client.predict(
+    		api_name="/fetch_toxic_tweets"
+    )
+    return result
+
+
+
+review_text=st.text_area("Text to reviewed")
+
 
 safety = st.slider('Personalize Safer Value: (larger value is less safe)',
-                   min_value=0.005,max_value=0.1,value=0.02,step=0.005)
+                        min_value=0.005,max_value=0.1,value=0.02,step=0.005)
 
-eval_result = get_toxic(review_text,safety)
-processed_metrics = process_metrics(eval_result[1])
 
-chart,metric = st.columns(2)
 
-with chart:
-    st.subheader('Rating')
-    st.pyplot(display_outcome((eval_result[0]['plot'][23::])))
-with metric:
-    st.subheader('Json Output')
-    st.dataframe(processed_metrics,column_config={'':'Key','value':'Value'})
+if st.button("Measure Text Toxicity", type="primary"):
+    eval_result = get_toxic(review_text,safety)
+    processed_metrics = process_metrics(eval_result[1])
+    img = eval_result[0]['plot'][23::]
+    chart,metric = st.columns(2)           
+    with chart:
+        st.subheader('Rating')
+        st.pyplot(display_outcome(img))
+    with metric:
+        st.subheader('Json Output')
+        st.dataframe(processed_metrics,column_config={'':'Key','value':'Value'})
+
+st.divider()  
+if st.button("Fetch Toxic Text", type="primary"):
+    toxic_tweets = get_tweets()
+    tweet = toxic_tweets[1]
+    #review_text = st.text_area("Text to reviewed",tweet)
+    eval_result = get_toxic(tweet,safety)
+    processed_metrics = process_metrics(eval_result[1])
+   
+   
+    img = eval_result[0]['plot'][23::]
+
+    text, chart,metric = st.columns(3)  
+    with text:
+        st.subheader('Fethed Text')
+        st.text(tweet)     
+    with chart:
+        st.subheader('Rating')
+        st.pyplot(display_outcome(img))
+    with metric:
+        st.subheader('Json Output')
+        st.dataframe(processed_metrics,column_config={'':'Key','value':'Value'})
+    st.subheader("")
+    st.html(toxic_tweets[0])
+    
+
+
+
+
+
+
+    
